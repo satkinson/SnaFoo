6e453cada624c559959ab5d6d2b5ef8609890c1c
Scott Atkinson <scotteatkinson@gmail.com>
2015-11-16 15:22:24 -0600

    added assets and header

--- SnaFoo/App_Start/BundleConfig.cs
+++ SnaFoo/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace SnaFoo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/assets/styles/modern.css"));
         }
     }
 }
