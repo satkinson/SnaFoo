81682fe8e106391d8dcffea3b9f954e6e1119d0d
Scott Atkinson <scotteatkinson@gmail.com>
2015-11-17 11:29:31 -0600

    implemented basic adding of suggestion

--- SnaFoo/App_Start/BundleConfig.cs
+++ SnaFoo/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace SnaFoo
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.cookie.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
