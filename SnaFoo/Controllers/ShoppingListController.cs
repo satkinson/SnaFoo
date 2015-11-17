b06c6474f793e46cd4accdc5e3acb50ba32bfde5
Scott Atkinson <scotteatkinson@gmail.com>
2015-11-17 16:25:47 -0600

    added Shopping LIst

---
+++ SnaFoo/Controllers/ShoppingListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SnaFoo.Controllers
+{
+    public class ShoppingListController : Controller
+    {
+        // GET: ShoppingList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
