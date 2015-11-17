cd3c97d7eae3a61074ff5233fdb77d6f268268ed
Scott Atkinson <scotteatkinson@gmail.com>
2015-11-16 20:07:13 -0600

    added knockout and optional always ordered items

--- SnaFoo/App_Start/BundleConfig.cs
+++ SnaFoo/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace SnaFoo
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                    "~/Scripts/knockout-3.3.0.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
