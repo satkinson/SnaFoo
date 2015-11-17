247a0cc0eca3de694f094b31dac62a3c968ed164
Scott Atkinson <scotteatkinson@gmail.com>
2015-11-16 18:23:55 -0600

    updated packages added suggestion controller

---
+++ SnaFoo/Controllers/SuggestionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SnaFoo.Controllers
+{
+    public class SuggestionController : Controller
+    {
+        // GET: Suggestion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SnaFoo/Models/Suggestion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnaFoo.Models
+{
+    public class Suggestion
+    {
+        public string SnackName { get; set; }
+        public string PurchaseLocation { get; set; }
+    }
+}
